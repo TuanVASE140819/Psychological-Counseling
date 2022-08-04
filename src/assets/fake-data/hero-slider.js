7f7441c6ff28ae8f998bb1e954c0e262b519b21a
Tuan <vuanhtuanthon2@gmail.com>
2022-08-04 10:00:02 +0700

    Create src

---
+++ src/assets/fake-data/hero-slider.js
@@ -0,0 +1,32 @@
+const img1 = require("../images/slider/slide_1.png").default;
+const img2 = require("../images/slider/slide_2.png").default;
+const img3 = require("../images/slider/slide_3.png").default;
+
+const heroSliderData = [
+  {
+    title: "Reetika Verma",
+    description:
+      " Thanks Astrotalk for being available at my difficult time and motivating me to get through that phase. I am finally doing good now after Navgrah Pooja performed by them. The complete process was very smooth. I'll recommend it to my friends. ",
+    img: img1,
+    color: "purple",
+    path: "/catalog/ao-thun-dinosaur-01"
+  },
+  {
+    title: "Dale Stricklin",
+    description:
+      " There is no comparison of Astrotalk when it comes to getting a free horoscope and checking horoscope online in the Astrology industry. They have revolutionized everything with excellent customer service by having a controlled marketplace for best astrologers from India. ",
+    img: img2,
+    path: "/catalog/ao-thun-dinosaur-02",
+    color: "pink"
+  },
+  {
+    title: "Gurdeep Chawla",
+    description:
+      " I really love the service of Astrotalk. Astrologers are really skilled and co-operative. Customer support team is very active and friendly. I get all answers and have a good conversation with astrologers at a decent price. And the predictions are also very accurate. ",
+    img: img3,
+    path: "/catalog/ao-thun-dinosaur-03",
+    color: "orange"
+  }
+];
+
+export default heroSliderData;
---
+++ src/assets/fake-data/policy.js
@@ -0,0 +1,24 @@
+const policy = [
+  {
+    name: "Chat with Astrologer",
+    description: "Miễn phí ship với đơn hàng > 239K",
+    icon: "bx bx-chat"
+  },
+  {
+    name: "Talk to Astrologer",
+    description: "Thanh toán khi nhận hàng (COD)",
+    icon: "bx bx-credit-card"
+  },
+  {
+    name: "Live Event",
+    description: "Ưu đãi dành cho khách hàng VIP",
+    icon: "bx bx-video-plus"
+  },
+  {
+    name: "Astrotalk Blog",
+    description: "Đổi, sửa đồ tại tất cả store",
+    icon: "bx bx-donate-heart"
+  }
+];
+
+export default policy;
