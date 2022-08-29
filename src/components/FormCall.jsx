87cae1de057e9657f07da3ffc7710babea4b2afc
Tuan <vuanhtuanthon2@gmail.com>
2022-08-29 11:52:42 +0700

    Create Form-Call

---
+++ src/components/FormCall.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function FormCall () {
+    return (
+        <div className='intake_from'>
+            {/* <div className='container'>
+                <ul className="profile-nav">
+                    <li>
+                        <i className="fa fa-home text-black home" />
+                    </li>
+                    <li>
+                        <a href="#" className="profile-address">Sapna's Profile</a>
+                    </li>
+                </ul>
+            </div> */}
+
+            <div className='row m-0'>
+              
+
+            </div>
+        </div>
+    )
+}
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -9,8 +9,10 @@ import Product from "../pages/Product";
 import Login from "../pages/Login";
 import PersonForm from "../pages/PersonForm";
 import Chat from "../components/Chat";
+import Talk from "../components/Talk";
 import Profile from "../pages/Profile";
 import FormChat from "../components/FormChat";
+import FormCall from "../components/FormCall";
 
 const Routes = () => {
   return (
@@ -24,6 +26,8 @@ const Routes = () => {
       <Route path="/chat" component={Chat} />
       <Route path="/profile" component={Profile} />
       <Route path='/formchat' component={FormChat} />
+      <Route path='/formcall' component={FormCall} />
+      <Route path='/talk' component={Talk} />
     </Switch>
   );
 };
