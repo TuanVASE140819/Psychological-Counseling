6f1ad318ec50bfa02fe0d37d98da8c47b7124fa9
Lun Lun <luanhua8888@gmail.com>
2022-09-16 01:56:31 +0700

    Create Login Register Page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -69,7 +69,9 @@ const Header = () => {
             <div className="header__menu__left__close" onClick={menuToggle}>
               <i className="bx bx-chevron-left"></i>
             </div>
+            
             {mainNav.map((item, index) => (
+              
               <div
                 key={index}
                 className={`header__menu__item header__menu__left__item ${
@@ -84,6 +86,11 @@ const Header = () => {
             ))}
           </div>
           <div className="header__menu__right">
+          <div className="header__menu__item header__menu__right__item">
+              <Link to="/signup">
+                <button className="btn-Sign-in" style={{fontSize:"22px"}}>SIGN IN</button>
+              </Link>
+            </div>
             <div className="header__menu__item header__menu__right__item">
               <i className="bx bx-search"></i>
             </div>
@@ -92,11 +99,7 @@ const Header = () => {
                 <i className="bx bx-shopping-bag"></i>
               </Link>
             </div>
-            <div className="header__menu__item header__menu__right__item">
-              <Link to="/login">
-                <i className="bx bx-user"></i>
-              </Link>
-            </div>
+            
           </div>
         </div>
       </div>
